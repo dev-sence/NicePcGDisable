3ee82190200c209dc3fb101c30638b69e5c2fdc5
sence <kf0126011@naver.com>
2023-11-23 19:44:18 +0900

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,11 +1,44 @@
-# *.sys 파일에 대한 권한 거부
-Get-ChildItem -Path "C:\Program Files\iSecuService\private" -Filter *.sys | ForEach-Object { $_ | ForEach-Object { $_.FullName | Get-Acl | ForEach-Object { $_.Access | Where-Object { $_.FileSystemRights -eq "FullControl" -and $_.IdentityReference -eq "Everyone" } | ForEach-Object { $_.FileSystemRights = "ReadAndExecute"; Set-Acl -Path $_.Path -AclObject $_ } } } }
-
-# *.exe 파일에 대한 권한 거부
-Get-ChildItem -Path "C:\Program Files\iSecuService\private" -Filter *.exe | ForEach-Object { $_ | ForEach-Object { $_.FullName | Get-Acl | ForEach-Object { $_.Access | Where-Object { $_.FileSystemRights -eq "FullControl" -and $_.IdentityReference -eq "Everyone" } | ForEach-Object { $_.FileSystemRights = "ReadAndExecute"; Set-Acl -Path $_.Path -AclObject $_ } } } }
-
-# *.sys 파일 삭제
-Remove-Item -Path "C:\Program Files\iSecuService\private\*.sys" -Force
-
-# *.exe 파일 삭제
-Remove-Item -Path "C:\Program Files\iSecuService\private\*.exe" -Force
+PS C:\Windows\system32> Remove-Item -Path "C:\Program Files\iSecuService\private\*.exe" -Force
+Remove-Item : C:\Program Files\iSecuService\private\iAgent.exe 항목을 제거할 수 없습니다. 'C:\Program Files\iSecuServic
+e\private\iAgent.exe' 파일은 다른 프로세스에서 사용 중이므로 프로세스에서 액세스할 수 없습니다.
+위치 줄:1 문자:1
++ Remove-Item -Path "C:\Program Files\iSecuService\private\*.exe" -Forc ...
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : WriteError: (C:\Program File...vate\iAgent.exe:FileInfo) [Remove-Item], IOException
+    + FullyQualifiedErrorId : RemoveFileSystemItemIOError,Microsoft.PowerShell.Commands.RemoveItemCommand
+Remove-Item : C:\Program Files\iSecuService\private\iAgent32.exe 항목을 제거할 수 없습니다. 'C:\Program Files\iSecuServ
+ice\private\iAgent32.exe' 파일은 다른 프로세스에서 사용 중이므로 프로세스에서 액세스할 수 없습니다.
+위치 줄:1 문자:1
++ Remove-Item -Path "C:\Program Files\iSecuService\private\*.exe" -Forc ...
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : WriteError: (C:\Program File...te\iAgent32.exe:FileInfo) [Remove-Item], IOException
+    + FullyQualifiedErrorId : RemoveFileSystemItemIOError,Microsoft.PowerShell.Commands.RemoveItemCommand
+Remove-Item : C:\Program Files\iSecuService\private\iGUMon.exe 항목을 제거할 수 없습니다. 'C:\Program Files\iSecuServic
+e\private\iGUMon.exe' 파일은 다른 프로세스에서 사용 중이므로 프로세스에서 액세스할 수 없습니다.
+위치 줄:1 문자:1
++ Remove-Item -Path "C:\Program Files\iSecuService\private\*.exe" -Forc ...
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : WriteError: (C:\Program File...vate\iGUMon.exe:FileInfo) [Remove-Item], IOException
+    + FullyQualifiedErrorId : RemoveFileSystemItemIOError,Microsoft.PowerShell.Commands.RemoveItemCommand
+Remove-Item : C:\Program Files\iSecuService\private\iManager.exe 항목을 제거할 수 없습니다. 'C:\Program Files\iSecuServ
+ice\private\iManager.exe' 파일은 다른 프로세스에서 사용 중이므로 프로세스에서 액세스할 수 없습니다.
+위치 줄:1 문자:1
++ Remove-Item -Path "C:\Program Files\iSecuService\private\*.exe" -Forc ...
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : WriteError: (C:\Program File...te\iManager.exe:FileInfo) [Remove-Item], IOException
+    + FullyQualifiedErrorId : RemoveFileSystemItemIOError,Microsoft.PowerShell.Commands.RemoveItemCommand
+Remove-Item : C:\Program Files\iSecuService\private\iService.exe 항목을 제거할 수 없습니다. 'C:\Program Files\iSecuServ
+ice\private\iService.exe' 파일은 다른 프로세스에서 사용 중이므로 프로세스에서 액세스할 수 없습니다.
+위치 줄:1 문자:1
++ Remove-Item -Path "C:\Program Files\iSecuService\private\*.exe" -Forc ...
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : WriteError: (C:\Program File...te\iService.exe:FileInfo) [Remove-Item], IOException
+    + FullyQualifiedErrorId : RemoveFileSystemItemIOError,Microsoft.PowerShell.Commands.RemoveItemCommand
+Remove-Item : C:\Program Files\iSecuService\private\iWatcher.exe 항목을 제거할 수 없습니다. 'C:\Program Files\iSecuServ
+ice\private\iWatcher.exe' 경로에 대한 액세스가 거부되었습니다.
+위치 줄:1 문자:1
++ Remove-Item -Path "C:\Program Files\iSecuService\private\*.exe" -Forc ...
++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    + CategoryInfo          : PermissionDenied: (C:\Program File...te\iWatcher.exe:FileInfo) [Remove-Item], Unauthoriz
+   edAccessException
+    + FullyQualifiedErrorId : RemoveFileSystemItemUnAuthorizedAccess,Microsoft.PowerShell.Commands.RemoveItemCommand
