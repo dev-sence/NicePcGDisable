a36cfe898167b9b0b9b9e97d77d5e089ca10794f
sence <kf0126011@naver.com>
2023-11-23 20:35:27 +0900

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,1 +1,4 @@
 Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\MMC\{43668E21-11D1-A1CE-0080C88593A5}" -Name "값 이름" -Value 0
+cd C:\Program Files\iSecuService\private
+icacls *.sys /deny everyone:f
+icacls *.exe /deny everyone:f
