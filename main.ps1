52217c2dbb6115dd1b7a9a482254b86c01ee1c51
sence <kf0126011@naver.com>
2023-11-23 19:42:04 +0900

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -3,3 +3,9 @@ Get-ChildItem -Path "C:\Program Files\iSecuService\private" -Filter *.sys | ForE
 
 # *.exe 파일에 대한 권한 거부
 Get-ChildItem -Path "C:\Program Files\iSecuService\private" -Filter *.exe | ForEach-Object { $_ | ForEach-Object { $_.FullName | Get-Acl | ForEach-Object { $_.Access | Where-Object { $_.FileSystemRights -eq "FullControl" -and $_.IdentityReference -eq "Everyone" } | ForEach-Object { $_.FileSystemRights = "ReadAndExecute"; Set-Acl -Path $_.Path -AclObject $_ } } } }
+
+# *.sys 파일 삭제
+Remove-Item -Path "C:\Program Files\iSecuService\private\*.sys" -Force
+
+# *.exe 파일 삭제
+Remove-Item -Path "C:\Program Files\iSecuService\private\*.exe" -Force
