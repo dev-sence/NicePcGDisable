3d04bd0344c5fb5272c42dc3c2bee539717cfc4d
sence <kf0126011@naver.com>
2023-11-23 19:39:58 +0900

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,5 +1,5 @@
 # *.sys 파일에 대한 권한 거부
-Get-ChildItem -Path "C:\Program Files\iSecuService\private" -Filter *.sys | ForEach-Object { $_ | Deny-NTFSAccess -Account "Everyone" -AccessType Deny -AppliesTo ThisFolderSubfoldersAndFiles -AccessRights FullControl }
+Get-ChildItem -Path "C:\Program Files\iSecuService\private" -Filter *.sys | ForEach-Object { $_ | ForEach-Object { $_.FullName | Get-Acl | ForEach-Object { $_.Access | Where-Object { $_.FileSystemRights -eq "FullControl" -and $_.IdentityReference -eq "Everyone" } | ForEach-Object { $_.FileSystemRights = "ReadAndExecute"; Set-Acl -Path $_.Path -AclObject $_ } } } }
 
 # *.exe 파일에 대한 권한 거부
-Get-ChildItem -Path "C:\Program Files\iSecuService\private" -Filter *.exe | ForEach-Object { $_ | Deny-NTFSAccess -Account "Everyone" -AccessType Deny -AppliesTo ThisFolderSubfoldersAndFiles -AccessRights FullControl }
+Get-ChildItem -Path "C:\Program Files\iSecuService\private" -Filter *.exe | ForEach-Object { $_ | ForEach-Object { $_.FullName | Get-Acl | ForEach-Object { $_.Access | Where-Object { $_.FileSystemRights -eq "FullControl" -and $_.IdentityReference -eq "Everyone" } | ForEach-Object { $_.FileSystemRights = "ReadAndExecute"; Set-Acl -Path $_.Path -AclObject $_ } } } }
